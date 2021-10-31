7b612eb930385b76d5923b987a87cdc1ad837a6e
Nikhil VJ <answerquest@users.noreply.github.com>
2021-10-31 14:50:14 +0530

    update db schema

--- dbschema.sql
+++ dbschema.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users(
 	username VARCHAR(50) NOT NULL PRIMARY KEY,
+	email VARCHAR(100) NULL,
 	role VARCHAR(32) NULL,
-	token VARCHAR(128) NULL,
+	token VARCHAR(100) NULL,
 	last_login DATETIME NULL,
 	pwd VARCHAR(1900) NULL,
 	status VARCHAR(32) NULL,
@@ -13,11 +14,12 @@ CREATE TABLE users(
 CREATE INDEX users_i1 ON users (token);
 CREATE INDEX users_i2 ON users (role);
 
+
 DROP TABLE IF EXISTS saplings;
 CREATE TABLE saplings(
-	id VARCHAR(32) NOT NULL PRIMARY KEY,
-	lat DECIMAL(11,8) NULL,
-	lon DECIMAL(11,8) NULL,
+	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	lat DECIMAL(9,6) NULL,
+	lon DECIMAL(9,6) NULL,
 	name VARCHAR(100) NULL,
 	`group` VARCHAR(32) NULL,
 	local_name VARCHAR(100) NULL,
@@ -30,7 +32,8 @@ CREATE TABLE saplings(
 	created_on DATETIME NULL,
 	created_by VARCHAR(32) NULL,
 	modified_on DATETIME NULL,
-	modified_by VARCHAR(32) NULL
+	modified_by VARCHAR(32) NULL,
+	CONSTRAINT saplings_c1 UNIQUE (lat, lon)
 );
 CREATE INDEX saplings_i1 ON saplings (planted_date);
 CREATE INDEX saplings_i2 ON saplings (status);
@@ -39,34 +42,36 @@ CREATE INDEX saplings_i4 ON saplings (`group`);
 CREATE INDEX saplings_i5 ON saplings (name);
 
 
-DROP TABLE IF EXISTS sponsor_sapling;
-CREATE TABLE sponsor_sapling(
-	id VARCHAR(32) NOT NULL PRIMARY KEY,
+DROP TABLE IF EXISTS adoptions;
+CREATE TABLE adoptions(
+	id VARCHAR(36) NOT NULL PRIMARY KEY,
 	username VARCHAR(32) NULL,
-	sapling_id VARCHAR(32) NULL,
-	status VARCHAR(32) NULL,
+	sapling_id VARCHAR(36) NULL,
+	adopted_name VARCHAR(32) NULL,
 	comments VARCHAR(255) NULL,
+	status VARCHAR(32) NULL,
 	approver VARCHAR(32) NULL,
 	application_date DATE NULL,
 	approval_date DATE NULL,
 	created_on DATETIME NULL,
 	created_by VARCHAR(32) NULL,
 	modified_on DATETIME NULL,
-	modified_by VARCHAR(32) NULL
+	modified_by VARCHAR(32) NULL,
+	CONSTRAINT adoptions_c1 UNIQUE (username, sapling_id)
 );
-CREATE INDEX sponsor_sapling_i1 ON sponsor_sapling (username);
-CREATE INDEX sponsor_sapling_i2 ON sponsor_sapling (sapling_id);
-CREATE INDEX sponsor_sapling_i3 ON sponsor_sapling (status);
-CREATE INDEX sponsor_sapling_i4 ON sponsor_sapling (approver);
-CREATE INDEX sponsor_sapling_i5 ON sponsor_sapling (application_date);
-CREATE INDEX sponsor_sapling_i6 ON sponsor_sapling (approval_date);
+CREATE INDEX adoptions_i1 ON adoptions (username);
+CREATE INDEX adoptions_i2 ON adoptions (sapling_id);
+CREATE INDEX adoptions_i3 ON adoptions (status);
+CREATE INDEX adoptions_i4 ON adoptions (approver);
+CREATE INDEX adoptions_i5 ON adoptions (application_date);
+CREATE INDEX adoptions_i6 ON adoptions (approval_date);
 
 
 DROP TABLE IF EXISTS observations;
 CREATE TABLE observations(
-	id VARCHAR(32) NOT NULL PRIMARY KEY,
-	sapling_id VARCHAR(32) NULL,
-	photo_id VARCHAR(32) NULL,
+	id VARCHAR(36) NOT NULL PRIMARY KEY,
+	sapling_id VARCHAR(36) NULL,
+	photo_id VARCHAR(36) NULL,
 	observation_date DATE NULL,
 	details TEXT NULL,
 	status VARCHAR(32) NULL,
